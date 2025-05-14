2958f49139a2a22ffa2756f1597dc0cb105156c3
sohampandit <panditsoham1@gmail.com>
2025-05-14 11:20:45 +0530

    commit

---
+++ src/InsertionSort.java
@@ -0,0 +1,23 @@
+package Sorting;
+import java.util.*;
+public class InsertionSort {
+    public static void Sort(int arr[]){
+        for (int i = 1; i < arr.length; i++) {
+            int curr=arr[i];
+            int prev=i-1;
+            while(prev>=0 && arr[prev]>curr){
+                arr[prev+1]=arr[prev];
+                prev--;
+            }
+
+            arr[prev+1]=curr;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int arr[]={3,1,2,5,6,4};
+        Sort(arr);
+        System.out.println("Sorted Array: "+Arrays.toString(arr));
+    }
+}
