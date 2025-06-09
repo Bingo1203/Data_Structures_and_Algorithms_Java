52ed2f486adc222c37a67a18df6e0cdf338d622e
sohampandit <panditsoham1@gmail.com>
2025-06-09 23:07:59 +0530

    commit

---
+++ src/TwoDArrays/creation.java
@@ -0,0 +1,25 @@
+package TwoDArrays;
+import java.util.*;
+public class creation {
+    public static void show(int[][] matrix){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int matrix[][]= new int[3][3];
+        int n=matrix.length, m=matrix[0].length;
+
+        Scanner sc= new Scanner(System.in);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        show(matrix);
+    }
+}
