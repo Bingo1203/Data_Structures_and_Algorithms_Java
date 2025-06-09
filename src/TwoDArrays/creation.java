4957e2dd39360c7d4b22c61f20fba32def2e9eec
sohampandit <panditsoham1@gmail.com>
2025-06-09 23:11:49 +0530

    commit

--- src/TwoDArrays/creation.java
+++ src/TwoDArrays/creation.java
@@ -9,6 +9,18 @@ public class creation {
             System.out.println();
         }
     }
+
+    public static boolean find(int[][] matrix, int key){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if(matrix[i][j]==key){
+                    System.out.println(key+" found at ("+i+","+j+")");
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
         int matrix[][]= new int[3][3];
         int n=matrix.length, m=matrix[0].length;
@@ -21,5 +33,6 @@ public class creation {
         }
 
         show(matrix);
+        find(matrix,7);
     }
 }
