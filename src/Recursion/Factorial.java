c1297555eadf9a95e0073940fd09c03eb4a793ee
sohampandit <panditsoham1@gmail.com>
2025-08-11 00:42:24 +0530

    commit

---
+++ src/Recursion/Factorial.java
@@ -0,0 +1,18 @@
+package Recursion;
+import java.util.*;
+public class Factorial {
+    public static int fact(int n){
+        if (n == 0) {
+            return 1;
+        }
+        int fnm1=fact(n-1);
+        int f=n*fact(n-1);
+        return f;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(fact(n));
+    }
+}
